0179ce605d35e4203dcbb2e6872ebba5a568e0dc
asin0128 <asin0128@student.monash.edu>
2023-10-07 15:58:28 +1100

    Start

--- mode1.py
+++ mode1.py
@@ -9,22 +9,23 @@ class Mode1Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.islands = islands
+        self.crew = crew
 
     def select_islands(self) -> list[tuple[Island, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def select_islands_from_crew_numbers(self, crew_numbers: list[int]) -> list[float]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def update_island(self, island: Island, new_money: float, new_marines: int) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        raise NotImplementedError
